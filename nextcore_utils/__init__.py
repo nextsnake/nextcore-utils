029672c6c7e25856c11ab7e1aefd095c8660ec41
THEGOLDENPRO <66202304+THEGOLDENPRO@users.noreply.github.com>
2023-08-31 22:45:20 +0100

    added nextcore_utils package

---
+++ nextcore_utils/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Final, Tuple
+
+__all__: Final[Tuple[str, ...]] = ()
+__version__: Final[str] = "1.0.0"
